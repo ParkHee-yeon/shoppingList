fed360ef1f51a0fea3939a9d0fdbf2be973948dd
heeyeon <heeyeon8662@pineone.com>
2023-10-19 15:55:48 +0900

    feature_header code

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
+      <h2>수정본임다 ~</h2>
       <Link to="/">홈</Link>
       <Link to="/cart">장바구니</Link>
     </header>
